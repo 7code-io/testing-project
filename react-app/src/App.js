46bb63fde123e8093c64b982148a21d89fa14075
7code.io <97501694+7code-io@users.noreply.github.com>
2023-07-22 17:29:36 -0400

    Add login page (#1)
    * Add login page
    
    * update for review

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/Login"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Testing React Project
-        </a>
-      </header>
+      <Login></Login>
     </div>
   );
 }
---
+++ react-app/src/pages/Login.js
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div>
+      <h1>Update our Login Page based on Review</h1>
+    </div>
+  );
+}
+
+export default Login;
